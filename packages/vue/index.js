c3454a862c8ddba8b1a6b40bd39baec36a43fed1
Igor Jacaúna <igor.jacauna@meliuz.com.br>
2022-06-20 13:04:45 -0400

    fix: Corrige regras para vue

--- packages/vue/index.js
+++ packages/vue/index.js
@@ -1,4 +1,13 @@
 module.exports = {
+  overrides: [
+    {
+      files: ['*.vue'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+    },
+  ],
   extends: [
     'plugin:vue/vue3-recommended',
     '@igorjacauna/eslint-config-typescript',
