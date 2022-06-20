dc8b1e604f2391a109e54f2cf555061043d2c90b
Igor Jacaúna <igor.jacauna@meliuz.com.br>
2022-06-20 09:06:35 -0400

    chore: Init

---
+++ packages/vue/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    '@igorjacauna/eslint-config-typescript',
+    'plugin:vue/vue3-recommended'
+  ],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+  }
+}
