3c11821fda20d4ab440bb17ff73f893507953e20
Igor Jacaúna <igor.jacauna@gmail.com>
2023-03-03 16:22:29 -0400

    fix: Corrige dependencia

--- packages/all/index.js
+++ packages/all/index.js
@@ -1,18 +1,5 @@
 module.exports = {
   extends: [
-    '@igorjacauna/eslint-config-basic',
-    'plugin:import/typescript',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  rules: {
-    'indent': ['error', 2],
-    'comma-dangle': ['error', {
-      'arrays': 'always-multiline',
-      'objects': 'always-multiline',
-      'imports': 'never',
-      'exports': 'never',
-      'functions': 'never'
-    }],
-    'semi': ['error', 'always']
-  }
+    '@igorjacauna/eslint-config-vue',
+  ]
 }
